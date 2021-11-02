e65feb00e012979aaea8ff9474c7bc1564ac91a0
YangDong-E <ehddh125@naver.com>
2021-11-02 15:22:04 +0900

    featrue/main add layout add function infinitescroll

---
+++ glog-client/components/Comment.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Comment({item: {id, email, body}}) {
+    return (
+        <div className="container">
+            <div className="content" style={{ height:500,width:350,float:'left'}}>
+                <div className="content-body" 
+                style={{textAlign:'center',padding:60,paddingLeft: 2,paddingRight: 2,border: 2,borderStyle:'solid',margin:50,float:'left',height:320}}>
+                <h5 className="content-title">Id : {id}</h5>
+                <h6 className="content-subtitle">Email : {email}</h6>
+                <p className="content-text">Comment : {body}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Comment
