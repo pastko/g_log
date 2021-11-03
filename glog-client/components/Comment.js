b31c429a20452937f8ff01b7a9d324f6eb0fa3e6
YangDong-E <ehddh125@naver.com>
2021-11-03 17:57:30 +0900

    feautre/main change function infiniteScroll

--- glog-client/components/Comment.js
+++ glog-client/components/Comment.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Comment({item: {id, email, body}}) {
     return (
-        <div className="container">
+        <div>
             <div className="content" style={{ height:500,width:350,float:'left'}}>
                 <div className="content-body" 
                 style={{textAlign:'center',padding:60,paddingLeft: 2,paddingRight: 2,border: 2,borderStyle:'solid',margin:50,float:'left',height:320}}>
--- glog-client/pages/Main.js
+++ glog-client/pages/Main.js
@@ -1,66 +1,82 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import Comment from "../components/Comment";
+import Comment from '../components/Comment';
 
 const Main = () => {
-
-    const[items,setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
     const [noMore, setnoMore] = useState(true);
 
     const [page, setpage] = useState(2);
 
-    useEffect(()=>{
+    useEffect(() => {
         const getComments = async () => {
-            const res = await fetch(
-                `http://localhost:3004/comments?_page=1&_limit=20`
-            );
+            // const res = await axios.get(
+            //     `http://localhost:3004/comments?_page=1&_limit=20`
+            // );
+            // const data = await res.json();
+            // setItems(data);
+            try {
+                return await axios.get(
+                    `http://localhost:3004/comments?_page=1&_limit=20`
+                );
+            } catch (error) {
+                console.error(error);
+            }
             const data = await res.json();
             setItems(data);
         };
 
         getComments();
-    },[]);
+    }, []);
 
     console.log(items);
 
-    const fetchComments = async() =>{
-        const res = await fetch(
-            `http://localhost:3004/comments?_page=${page}&_limit=20`
-        );
+    const fetchComments = async () => {
+        // const res = await axios.get(
+        //     `http://localhost:3004/comments?_page=${page}&_limit=20`
+        // );
+        // const data = await res.json();
+        // return data;
+        try {
+            return await axios.get(
+                `http://localhost:3004/comments?_page=${page}&_limit=20`
+            );
+        } catch (error) {
+            console.log(error);
+        }
         const data = await res.json();
         return data;
     };
     const fetchData = async () => {
-    const commentsFormServer = await fetchComments();
-    setItems([...items, ...commentsFormServer]);
+        const commentsFormServer = await fetchComments();
+        setItems([...items, ...commentsFormServer]);
 
-    if(commentsFormServer.length === 0 || commentsFormServer.length < 20){
-    setnoMore(false);
-    }
+        if (commentsFormServer.length === 0 || commentsFormServer.length < 20) {
+            setnoMore(false);
+        }
 
-    setpage(page + 1);
-    console.log('hello');
-
+        setpage(page + 1);
+        console.log('hello');
     };
 
     return (
-    
         <InfiniteScroll
-  dataLength={items.length} //This is important field to render the next data
-    next={fetchData}
-    hasMore={noMore}
-    loader={<h4>Loading...</h4>}
-    endMessage={
-    <p style={{ textAlign: 'center' }}>
-    <b></b>
-    </p>
-}
->
-    {items.map((item)=>{
-    return <Comment key={item.id} item={item} />
-    })}
-</InfiniteScroll>
+            dataLength={items.length} //This is important field to render the next data
+            next={fetchData}
+            hasMore={noMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <p style={{ textAlign: 'center' }}>
+                    <b></b>
+                </p>
+            }
+        >
+            {items.map((item) => {
+                return <Comment key={item.id} item={item} />;
+            })}
+        </InfiniteScroll>
     );
-}
+};
 export default Main;
