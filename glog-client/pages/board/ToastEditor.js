9649dd28a9d36854ca7049a68ae249d49ca636ac
hwlee-study <hwlee8042@gmail.com>
2021-11-07 15:07:07 +0900

    add toast editor

---
+++ glog-client/pages/board/ToastEditor.js
@@ -0,0 +1,20 @@
+import dynamic from 'next/dynamic';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+
+function ToastEditor() {
+
+  const Editor = dynamic(() => import('@toast-ui/react-editor').then(m => m.Editor), { ssr: false });
+
+  return (
+    <Editor
+      nitialValue="hello react editor world!"
+      previewStyle="vertical"
+      height="600px"
+      initialEditType="markdown"
+      useCommandShortcut={true}
+    />
+  );
+}
+
+export default ToastEditor;
