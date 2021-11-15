b1d8f8ada00133fc84ee4903a41bab9f39385a38
hwlee-study <hwlee8042@gmail.com>
2021-11-15 15:48:42 +0900

    proxy 수정

--- glog-client/src/pages/Home.js
+++ glog-client/src/pages/Home.js
@@ -1,7 +1,4 @@
-import React, {
-    useEffect,
-    useState
-} from 'react';
+import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Post from '../components/main/Post';
 import styled from 'styled-components';
@@ -15,66 +12,58 @@ const Home = () => {
     // useEffect(() => {
     //     const getComments = async () => {
     //         const res = await axios.get(
-    //             '/board?pageNum=0&sortRule=1',{
-    //                 header : {
-    //                     authorization:"",
-    //                     'X-USER-ID':""
-    //                 }
+    //             '/board?pageNum=0&sortRule=1', {
+    //             header: {
+    //                 authorization: "",
+    //                 'X-USER-ID': ""
     //             }
+    //         }
     //         );
     //         setPosts(res.data);
     //     };
     //     getComments();
     // }, []);
 
-    const fetchComments = async () => {
-        const res = await axios.get(
-            '/board?pageNum={page}&sortRule=1', {
-                header: {
-                    authorization: "",
-                    'X-USER-ID': ""
-                }
-            }
-        );
-        return res.data;
-    };
+    // const fetchComments = async () => {
+    //     const res = await axios.get(
+    //         '/board?pageNum={page}&sortRule=1', {
+    //         header: {
+    //             authorization: "",
+    //             'X-USER-ID': ""
+    //         }
+    //     }
+    //     );
+    //     return res.data;
+    // };
 
-    const fetchData = async () => {
-        const postsFormServer = await fetchComments();
-        setPosts([...posts, ...postsFormServer]);
+    // const fetchData = async () => {
+    //     const postsFormServer = await fetchComments();
+    //     setPosts([...posts, ...postsFormServer]);
 
-        if (postsFormServer.length === 0 || postsFormServer.length < 20) {
-            setnoMore(false);
-        }
-        setpage(page + 1);
-    };
+    //     if (postsFormServer.length === 0 || postsFormServer.length < 20) {
+    //         setnoMore(false);
+    //     }
+    //     setpage(page + 1);
+    // };
 
-    return ( <
-        >
-        <
-        StyleInfiniteScroll >
-        <
-        InfiniteScroll dataLength = {
-            posts.length
-        } //This is important field to render the next data
-        next = {
-            fetchData
-        }
-        hasMore = {
-            noMore
-        } >
-        {
-            /* {Post.length === 0 ? 'Emtpy' : posts.map((post) => {
-                                    return <Post key={posts.bord_idx} post={post} />;
-                                })} */
-        } <
-        /InfiniteScroll> <
-        /StyleInfiniteScroll> <
-        />
+    return (
+        <>
+            <StyleInfiniteScroll>
+                <InfiniteScroll
+                    dataLength={posts.length} //This is important field to render the next data
+                    // next={fetchData}
+                    hasMore={noMore}
+                >
+                    {/* {Post.length === 0 ? 'Emtpy' : posts.map((post) => {
+                        return <Post key={posts.bord_idx} post={post} />;
+                    })} */}
+                </InfiniteScroll>
+            </StyleInfiniteScroll>
+        </>
     );
 };
 
-const StyleInfiniteScroll = styled.div `
+const StyleInfiniteScroll = styled.div`
     max-width: 1280px;
     margin: 20px auto;
     position: flex;
