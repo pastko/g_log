d8fe9d7ea14c3e66b1c7d63d31aa742967ae885b
bey4166 <bey8469@gmail>
2021-11-15 18:28:49 +0900

    add comment crud

--- glog-server/src/main/java/com/gteam/glog/comment/controller/CommentController.java
+++ glog-server/src/main/java/com/gteam/glog/comment/controller/CommentController.java
@@ -1,16 +1,40 @@
 package com.gteam.glog.comment.controller;
 
 import com.gteam.glog.comment.service.CommentService;
+import com.gteam.glog.common.utils.ResponseDTOUtils;
+import com.gteam.glog.domain.dto.comment.CommentRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class CommentController {
 
     private final CommentService commentService;
+    private final ResponseDTOUtils responseDTOUtils;
 
     @Autowired
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, ResponseDTOUtils responseDTOUtils) {
         this.commentService = commentService;
+        this.responseDTOUtils = responseDTOUtils;
+    }
+
+    @PostMapping(name = "/comtwrite")
+    public ResponseEntity<?> createComment(@RequestBody CommentRequestDTO request ,@RequestHeader("X-USR-ID") String id, HttpServletResponse response) {
+        return responseDTOUtils.doGenerateResponseDTO(commentService.createComment(request, id));
+    }
+    @GetMapping(name = "/comtupdate")
+    public ResponseEntity<?> updateComment(@RequestParam("comt_idx") int comt_idx) {
+        return responseDTOUtils.doGenerateResponseDTO(commentService.updateComment(comt_idx));
+    }
+    @GetMapping(name = "/comt")
+    public ResponseEntity<?> detailComment(@RequestParam("bord_idx") int bord_idx) {
+        return responseDTOUtils.doGenerateResponseDTO(commentService.detailComment(bord_idx));
+    }
+    @PostMapping(name = "/comtdelete")
+    public ResponseEntity<?> deleteComment(@RequestBody int comt_idx) {
+        return responseDTOUtils.doGenerateResponseDTO(commentService.deleteComment(comt_idx));
     }
 }
--- glog-server/src/main/java/com/gteam/glog/domain/dto/comment/CommentContentsDTO.java
+++ glog-server/src/main/java/com/gteam/glog/domain/dto/comment/CommentContentsDTO.java
@@ -3,9 +3,14 @@ package com.gteam.glog.domain.dto.comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
+
 @Getter
 @Setter
 public class CommentContentsDTO {
     private String nikNm;
     private String content;
+    private LocalDate createDt;
+    private int bord_idx;
 }
